63e1c92a8c3d2eb2f3a913ef84003bf2d04a4742
kova0522 <kovarik.balint@gmail.com>
2022-11-04 11:12:07 +0100

    színválasztás

--- Absztrakt/Absztrakt/Form1.cs
+++ Absztrakt/Absztrakt/Form1.cs
@@ -76,5 +76,16 @@ namespace Absztrakt
         {
             Factory = new BallFactory();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
+        }
     }
 }
