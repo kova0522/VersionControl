afaa1db502c29cf5b28444e67a882304f03f6ee7
Bálint Kovarik <kovarik.balint@gmail.com>
2022-11-04 18:14:20 +0100

    present

--- Absztrakt/Absztrakt/Form1.Designer.cs
+++ Absztrakt/Absztrakt/Form1.Designer.cs
@@ -37,14 +37,18 @@ namespace Absztrakt
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
             this.mainPanel.AutoSize = true;
-            this.mainPanel.Location = new System.Drawing.Point(72, 51);
+            this.mainPanel.Location = new System.Drawing.Point(108, 78);
+            this.mainPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(465, 289);
+            this.mainPanel.Size = new System.Drawing.Size(698, 445);
             this.mainPanel.TabIndex = 0;
             // 
             // createTimer
@@ -61,9 +65,10 @@ namespace Absztrakt
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Location = new System.Drawing.Point(20, 20);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 35);
             this.button1.TabIndex = 1;
             this.button1.Text = "CAR";
             this.button1.UseVisualStyleBackColor = true;
@@ -71,9 +76,10 @@ namespace Absztrakt
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(94, 12);
+            this.button2.Location = new System.Drawing.Point(141, 18);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(112, 35);
             this.button2.TabIndex = 2;
             this.button2.Text = "BALL";
             this.button2.UseVisualStyleBackColor = true;
@@ -82,31 +88,70 @@ namespace Absztrakt
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(176, 21);
+            this.label1.Location = new System.Drawing.Point(264, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.Size = new System.Drawing.Size(101, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "Coming next:";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(321, 13);
+            this.button3.Location = new System.Drawing.Point(482, 20);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(112, 35);
             this.button3.TabIndex = 4;
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(800, 20);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 35);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "PRESENT";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(926, 20);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 35);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "BoxColor";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1046, 20);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(112, 35);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "RibbonColor";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.mainPanel);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -123,6 +168,9 @@ namespace Absztrakt
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
---
+++ Absztrakt/Absztrakt/entities/Present.cs
@@ -0,0 +1,28 @@
+﻿using Absztrakt.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absztrakt.entities
+{
+    public class Present : Toy
+    {
+        public SolidBrush RibbonColor { get; private set; }
+        public SolidBrush BoxColor { get; private set; }
+        public Present(Color box, Color ribbon)
+        {
+            BoxColor = new SolidBrush(box);
+            RibbonColor = new SolidBrush(ribbon);
+        }
+
+        protected override void DrawImage(Graphics g)
+        {
+            g.FillRectangle(BoxColor, 0, 0, Width, Height);
+            g.FillRectangle(RibbonColor, 20, 0, Width/5, Height);
+            g.FillRectangle(RibbonColor, 0, 20, Width, Height/5);
+        }
+    }
+}
