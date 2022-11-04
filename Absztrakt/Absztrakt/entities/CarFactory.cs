c1a4807d54c778ea2a637e33e530b7a36077fb51
kova0522 <kovarik.balint@gmail.com>
2022-11-04 11:03:55 +0100

    abstraction use

--- Absztrakt/Absztrakt/entities/CarFactory.cs
+++ Absztrakt/Absztrakt/entities/CarFactory.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace Absztrakt.entities
 {
-    public class BallFactory: IToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
