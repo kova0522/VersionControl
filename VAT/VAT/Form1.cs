9f00d9b3c66ec2eb21f931132fd2051048afc926
kova0522 <kovarik.balint@gmail.com>
2022-11-11 10:49:18 +0100

    mentés

--- VAT/VAT/Form1.cs
+++ VAT/VAT/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ namespace VAT
         PortfolioEntities context = new PortfolioEntities();
         List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         List<Tick> Ticks;
+        List<decimal> Nyereségek = new List<decimal>();
 
         public Form1()
         {
@@ -24,7 +26,7 @@ namespace VAT
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
 
-            List<decimal> Nyereségek = new List<decimal>();
+
             int intervalum = 30;
             DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
             DateTime záróDátum = new DateTime(2016, 12, 30);
@@ -67,6 +69,29 @@ namespace VAT
                 value += (decimal)last.Price * item.Volume;
             }
             return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                sw.Write("Időszak");
+                sw.Write(";");
+                sw.Write("Nyereség");
+                int counter = 0;
+                foreach (var s in Nyereségek)
+                {
+                    counter++;
+                    sw.WriteLine();
+                    sw.Write(counter);
+                    sw.Write(";");
+                    sw.Write(s.ToString());
+
+                }
+            }
         }
     }
 }
